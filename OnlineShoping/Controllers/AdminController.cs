6aa03beb83932bf37e8ab18800e461c23cb6fc9d
nguyenchithanh <nguyenchithanh281195@gmail.com>
2017-11-20 16:39:19 +0700

    add service

---
+++ OnlineShoping/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoping.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+
+        public ActionResult ListProduct()
+        {
+            return View();
+        }
+
+        public ActionResult ProductUpdate()
+        {
+            return View();
+        }
+    }
+}
---
+++ Service/WebService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Service
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod] //truoc moi ham can khai bao webmethod de ben ngoai co the goi duoc
+        public string HelloWorld() // sau do viet ham nhu binh thuowng
+        {
+            return "Hello World";
+        }
+    }
+}
